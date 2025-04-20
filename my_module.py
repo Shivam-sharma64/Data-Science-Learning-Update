0a2aab62c99830511e240452d65da82ae3e0d2a7
Shivam-sharma64 <sharmashivam.personal@gmail.com>
2025-04-20 14:15:13 +0530

    Add files via upload

---
+++ my_module.py
@@ -0,0 +1,11 @@
+data={"name":"data",
+"course":["data sciecnce","maths","python","machine learning"],
+"greeting":"greeting from data"
+}
+
+def get_course():
+	return data["course"]
+	
+def greeting():
+	return data["greeting"]
+	
---
+++ test2.py
@@ -0,0 +1,15 @@
+def add(a,b):
+    """this is a function which can return a addition"""
+    return a+b
+def substract(a,b):
+    """this is a function which can return a substract"""
+    return a-b 
+def multiply(a,b):
+    """this is a function which can return a multiply"""
+    return a*b
+def division(a,b):
+    """this is a function which can return a division"""
+    return a/b
+def test():
+    """this is nothing telling you what this extension is doing"""
+    return "this module is for add , substract,multiply and division"
